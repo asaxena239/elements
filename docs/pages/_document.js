bbef7d41597fff01d7dcd342457dff1233105d1f
Patrick Krawczykowski <patrick@dreadful.design>
2022-03-22 09:13:03 -0500

    remove render function from _document

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -1,19 +1,7 @@
-import Document, { Head, Html, Main, NextScript } from "next/document"
-import { Fragment } from "react"
+import Document from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
@@ -29,10 +17,10 @@ export default class MyDocument extends Document {
       return {
         ...initialProps,
         styles: (
-          <Fragment>
+          <>
             {initialProps.styles}
             {sheet.getStyleElement()}
-          </Fragment>
+          </>
         ),
       }
     } finally {
