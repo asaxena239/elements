52b7c51d6824629709f8b58e33b241414d2ff8d4
Patrick Krawczykowski <patrick@dreadful.design>
2022-03-22 09:13:09 -0500

    add todo

--- packages/codemod/transforms/update-imports.js
+++ packages/codemod/transforms/update-imports.js
@@ -67,6 +67,9 @@ module.exports = function (file, api) {
       }
       return j.importSpecifier(path.local, path.local)
     })
+
+    // TODO: Deal with typography changes differently. Remove indentifiers to Text and Heading and insert Box components.
+
     if (source.includes("@rent_avail")) {
       const newSource = j.literal(changePackage(source))
       const declaration = j.importDeclaration(specifiers, newSource)
