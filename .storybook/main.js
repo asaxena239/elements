931ecff1d99819d8e46c441d7cc379043b37a667
Patrick Krawczykowski <patrick@dreadful.design>
2022-04-28 10:22:34 -0500

    add accesibility monitoring

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,11 @@ module.exports = {
     "../packages/**/*.stories.@(js|jsx|ts|tsx)",
     "../patterns/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+  ],
   staticDirs: ["../public"],
   features: {
     postcss: false,
